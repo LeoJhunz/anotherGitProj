ce78e9bb41d450d50e959aa51002e948cf6443ad
leojhunz <leo.cawitjr@gmail.com>
2018-04-19 02:43:49 +0800

    initial commit
    initial commiting

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ public function do_login(){
+	// this will login the user
+ }
+ 
+ public function logout(){
+	// this will logout the user
+ }
+ 
+ ?>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Test 1
+
+For GitHub Windows GUI Client
